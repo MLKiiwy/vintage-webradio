fda784063ff05d36e7d952eb7ea415f61f2e8789
Mikael Labrut <labrut@gmail.com>
2018-09-20 21:37:18 +0200

    wip

--- daemon-scripts/listen-for-shutdown/daemon.py
+++ daemon-scripts/listen-for-shutdown/daemon.py
@@ -1,10 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# shutdown/reboot(/power on) Raspberry Pi with pushbutton
 
 import RPi.GPIO as GPIO
-]import subprocess
+from subprocess import call
+from datetime import datetime
+import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+# pushbutton connected to this GPIO pin, using pin 5 also has the benefit of
+# waking / powering up Raspberry Pi when button is pressed
+shutdownPin = 5
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+# if button pressed for at least this long then shut down. if less then reboot.
+shutdownMinSeconds = 3
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+buttonPressedTime = None
+
+def buttonStateChanged(pin):
+    global buttonPressedTime
+
+    if not (GPIO.input(pin)):
+        # button is down
+        if buttonPressedTime is None:
+            buttonPressedTime = datetime.now()
+    else:
+        # button is up
+        if buttonPressedTime is not None:
+            elapsed = (datetime.now() - buttonPressedTime).total_seconds()
+            buttonPressedTime = None
+            if elapsed >= shutdownMinSeconds:
+                # button pressed for more than specified time, shutdown
+                call(['shutdown', '-h', 'now'], shell=False)
+
+# subscribe to button presses
+GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged)
+
+while True:
+    # sleep to reduce unnecessary CPU usage
+    time.sleep(5)
