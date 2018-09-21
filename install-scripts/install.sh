4fb9d732ebaf9e6873cb31e2d0150bfba6ab6f35
Mikael Labrut <labrut@gmail.com>
2018-09-21 19:32:33 +0200

    wip

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -7,11 +7,11 @@ echo "enable_uart=1" >> /boot/config
 # Install the daemon listen-for-shutdown.py
 
 # Install the binary
-sudo mv ./daemon-scripts/listen-for-shutdown/daemon.py /usr/local/bin/listen-for-shutdown.py
+sudo cp ./daemon-scripts/listen-for-shutdown/daemon.py /usr/local/bin/listen-for-shutdown.py
 sudo chmod +x /usr/local/bin/listen-for-shutdown.py
 
 # Setup service
-sudo mv ./daemon-scripts/listen-for-shutdown/service.sh /etc/init.d/listen-for-shutdown.sh
+sudo cp ./daemon-scripts/listen-for-shutdown/service.sh /etc/init.d/listen-for-shutdown.sh
 sudo chmod +x /etc/init.d/listen-for-shutdown.sh
 
 # Now we'll register the script to run on boot.
--- install-scripts/update.sh
+++ install-scripts/update.sh
@@ -3,6 +3,6 @@
 # Update listen-for-shutdown.py
 /etc/init.d/listen-for-shutdown.sh stop
 rm /usr/local/bin/listen-for-shutdown.py
-mv ./daemon-scripts/listen-for-shutdown/daemon.py /usr/local/bin/listen-for-shutdown.py
+cp ./daemon-scripts/listen-for-shutdown/daemon.py /usr/local/bin/listen-for-shutdown.py
 chmod +x /usr/local/bin/listen-for-shutdown.py
 /etc/init.d/listen-for-shutdown.sh start
