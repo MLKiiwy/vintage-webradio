25620c2634a8be9f4fe768c706780b93e496d3e3
Mikael Labrut <labrut@gmail.com>
2018-09-21 19:59:37 +0200

    wip

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -19,3 +19,17 @@ sudo update-rc.d listen-for-shutdown.sh defaults
 
 sudo /etc/init.d/listen-for-shutdown.sh start
 
+# Install the daemon listen-volume.py
+
+# Install the binary
+sudo cp ./daemon-scripts/listen-volume/daemon.py /usr/local/bin/listen-volume.py
+sudo chmod +x /usr/local/bin/listen-volume.py
+
+# Setup service
+sudo cp ./daemon-scripts/listen-volume/service.sh /etc/init.d/listen-volume.sh
+sudo chmod +x /etc/init.d/listen-volume.sh
+
+# Now we'll register the script to run on boot.
+sudo update-rc.d listen-volume.sh defaults
+
+sudo /etc/init.d/listen-volume.sh start
--- install-scripts/update.sh
+++ install-scripts/update.sh
@@ -6,3 +6,10 @@ rm /usr/local/bin/listen-for-shutdown.py
 cp ./daemon-scripts/listen-for-shutdown/daemon.py /usr/local/bin/listen-for-shutdown.py
 chmod +x /usr/local/bin/listen-for-shutdown.py
 /etc/init.d/listen-for-shutdown.sh start
+
+# Update listen-volume.py
+/etc/init.d/listen-volume.sh stop
+rm /usr/local/bin/listen-volume.py
+cp ./daemon-scripts/listen-volume/daemon.py /usr/local/bin/listen-volume.py
+chmod +x /usr/local/bin/listen-volume.py
+/etc/init.d/listen-volume.sh start
