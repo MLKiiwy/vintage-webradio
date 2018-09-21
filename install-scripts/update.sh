af96e02150f0dd40352c494a8cd8d0ca10e21a41
Mikael Labrut <labrut@gmail.com>
2018-09-21 19:31:59 +0200

    wip

--- install-scripts/update.sh
+++ install-scripts/update.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 # Update listen-for-shutdown.py
-sudo /etc/init.d/listen-for-shutdown.sh stop
-sudo rm /usr/local/bin/listen-for-shutdown.py
-sudo mv ./daemon-scripts/listen-for-shutdown/daemon.py /usr/local/bin/listen-for-shutdown.py
-sudo /etc/init.d/listen-for-shutdown.sh start
+/etc/init.d/listen-for-shutdown.sh stop
+rm /usr/local/bin/listen-for-shutdown.py
+mv ./daemon-scripts/listen-for-shutdown/daemon.py /usr/local/bin/listen-for-shutdown.py
+chmod +x /usr/local/bin/listen-for-shutdown.py
+/etc/init.d/listen-for-shutdown.sh start
